5fa1cc1c36100ce8e0d23b7e42a228a7770afbd7
Jasleen Kaur <kaurjasleen1728@gmail.com>
2024-11-03 19:14:16 +0530

    updated temperature toogles

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-// alert("Hello");
-
-const { img } = require("framer-motion/client");
-
 function formatDate() {
   let cityTime = document.querySelector("#current-city-time");
   let today = new Date();
@@ -36,46 +32,37 @@ function formatDate() {
   ];
   cityTime.innerHTML = `${days[day]}, ${date} ${months[month]}`;
 }
-function searchCity(response) {
-  function displayTemperature(city) {
-    let actualTemperature = document.querySelector(".temperature-value");
-    actualTemperature.innerHTML = `${Math.round(
-      city.data.temperature.current
+function updateWeather(city) {
+  let actualTemperature = document.querySelector(".temperature-value");
+  actualTemperature.innerHTML = `${Math.round(city.data.temperature.current)}`;
+
+  let feelsLikeTemperature = document.querySelector(".temperature-feels-like");
+  // console.log(city.data.temperature.feels_like);
+  if (city.data.temperature.feels_like < 10)
+    feelsLikeTemperature.innerHTML = `0${Math.round(
+      city.data.temperature.feels_like
     )}`;
-
-    let feelsLikeTemperature = document.querySelector(
-      ".temperature-feels-like"
-    );
-    // console.log(city.data.temperature.feels_like);
-    if (city.data.temperature.feels_like < 10)
-      feelsLikeTemperature.innerHTML = `0${Math.round(
-        city.data.temperature.feels_like
-      )}`;
-    else {
-      feelsLikeTemperature.innerHTML = `${Math.round(
-        city.data.temperature.feels_like
-      )}`;
-    }
-
-    // not working yet
-    //let minTemperature = document.querySelector(
-    //   ".min-temperature .condition .values"
-    // );
-    // console.log(city.data.daily[0].temperature.minimum);
-    // minTemperature.innerHTML = `${city.data.daily[0].temperature.minimum}ºC`;
-
-    let windSpeed = document.querySelector(".wind-speed .condition .values");
-    windSpeed.innerHTML = `${city.data.wind.speed}km/h`;
-
-    let humidity = document.querySelector(".humidity .condition .values");
-    humidity.innerHTML = `${city.data.temperature.humidity}%`;
-
-    let weather_description = document.querySelector(".description");
-    weather_description.innerHTML = `${city.data.condition.description}`;
-    let weather_icon = document.querySelector(".current-city-weather-icon");
-    weather_icon.innerHTML = `<img src="${city.data.condition.icon_url}"
-                        alt="few-clouds-day">`;
+  else {
+    feelsLikeTemperature.innerHTML = `${Math.round(
+      city.data.temperature.feels_like
+    )}`;
   }
+  let windSpeed = document.querySelector(".wind-speed .condition .values");
+  windSpeed.innerHTML = `${city.data.wind.speed}km/h`;
+
+  let humidity = document.querySelector(".humidity .condition .values");
+  humidity.innerHTML = `${city.data.temperature.humidity}%`;
+
+  let weather_description = document.querySelector(".description");
+  weather_description.innerHTML = `${city.data.condition.description}`;
+  let weather_icon = document.querySelector(".current-city-weather-icon");
+  weather_icon.innerHTML = `<img src="${city.data.condition.icon_url}"
+                        alt="few-clouds-day">`;
+
+  formatDate();
+}
+
+function searchCity(response) {
   response.preventDefault();
   let apiKey = "2d8o4b96bdta6ee065c85fc43853285d";
   let cityElement = document.querySelector(".input-search-text");
@@ -83,17 +70,32 @@ function searchCity(response) {
   let cityName = document.querySelector("#city-name");
   cityName.innerHTML = cityElement;
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityElement}&key=${apiKey}`;
-  axios.get(apiUrl).then(displayTemperature);
-
-  //
-  // let weatherIcon = document.querySelector(".current-city-weather-icon");
-  // ;
-  // let maxTemperature = document.querySelector(
-  //   ".max-temperature .condition .values"
-  // );
-  // let windSpeed = document.querySelector("wind-speed .condition .values");
-  //
+  axios.get(apiUrl).then(updateWeather);
 }
+
 let searchButton = document.querySelector(".input-search-submit");
 searchButton.addEventListener("click", searchCity);
-formatDate();
+
+let temp_update = document.querySelectorAll(".temp-update");
+let temp_unit = document.querySelectorAll(".temp-unit");
+temp_update.forEach((temp) => {
+  temp.addEventListener("dblclick", () => {
+    convertAllTemperatures();
+  });
+});
+function convertAllTemperatures() {
+  let isCelsius = temp_unit[0].innerHTML === "ºC";
+  temp_update.forEach((temp) => {
+    let current_temp = parseFloat(temp.innerHTML);
+    isCelsius
+      ? (temp.innerHTML = Math.round((9 / 5) * current_temp + 32))
+      : (temp.innerHTML = Math.round((current_temp - 32) * (5 / 9)));
+  });
+
+  temp_unit.forEach((unit) => {
+    isCelsius ? (unit.innerHTML = "ºF") : (unit.innerHTML = "ºC");
+    
+  });
+}
+
+
